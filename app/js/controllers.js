c553229c435c032c535d4717f8f5b6cffb39e36c
irina694 <patrikeeva.irina@gmail.com>
2015-01-11 23:43:16 -0800

    Added Apple HQ Project description on Home page on rhs of video. Card #33.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -214,9 +214,9 @@ bildControllers.controller('CorporationsCtrl', ['$scope',
 bildControllers.controller('UserCtrl', ['$scope',
   function($scope) {
      // if ($scope.user != undefined) {
-      $scope.user = {};
-      $scope.user.name = 'Corporation';
-      $scope.user.logo = 'img/Corporation-logo.png';
+      // $scope.user = {};
+      // $scope.user.name = 'Corporation';
+      // $scope.user.logo = 'img/Corporation-logo.png';
     // }
      $scope.login = function() {
       $scope.user = {};
